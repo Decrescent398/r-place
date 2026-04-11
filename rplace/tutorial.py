89d54c072b09d6056bb732943b24e78594818875
Decrescent398 <ganeshhgunda123@gmail.com>
2026-04-11 17:28:30 +0530

    set up notion export for tutorial

--- rplace/tutorial.py
+++ rplace/tutorial.py
@@ -4,7 +4,16 @@ from rxconfig import config
 class State(rx.State):
     """The app state."""
     
+# https://www.florkofcows.com/ - add floating stickers after IP rights
+    
 def content() -> rx.Component:
     return rx.box(
-        rx.color_mode.button(position="top-right")
+        rx.el.iframe(
+            src="/tutorial/tutorial.html",
+            style={
+                "width": "100%",
+                "height": "100vh",
+                "border": "none",
+            },
+        ),
     )
